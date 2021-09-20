68ecc138ac1bd6f51047cf43a349e903fca79cff
Leo Chen <chenl3@rose-hulman.edu>
2021-09-20 18:29:29 -0400

    commit tree df9c193096267070e05c243d5e7d4f6e2c6797b3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(msg = "roller-skate"){
+    return `This Saturday, I want to ${msg}!`;
+}
+
+function mondayWork(activity = "go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(symbol="*") {
+    return function(phrase) {
+        return `You are ${symbol}${phrase}${symbol}!`;
+    }
+}
